1bc6f35c27243085c79e80d86f3d803b37cace4f
Nevzat <nevzatcelik151907@gmail.com>
2022-10-25 20:16:09 +0200

    JUnit

--- src/test/java/ders11_Cookies_WebTables/C01_Cookies.java
+++ src/test/java/ders11_Cookies_WebTables/C01_Cookies.java
@@ -1,13 +1,32 @@
 package ders11_Cookies_WebTables;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import utilities.TestBase;
 
 import java.util.Scanner;
+import java.util.Set;
 
-public class C01_Cookies {
+public class C01_Cookies extends TestBase {
 
     @Test
     public void test01(){
+        driver.get("https://www.youtube.com");
+        driver.findElement(By.xpath("//*[text()='Tümünü kabul et']")).click();
+
+        Set<Cookie> cookieSeti=driver.manage().getCookies();
+        for (Cookie eachCookie:cookieSeti
+             ) {
+            System.out.println(eachCookie);
+        }
+
+        System.out.println("============================================");
+
+        Cookie cookie=new Cookie("En guzel cookie","bizim cookie");
+        driver.manage().addCookie(cookie);
+        System.out.println(cookie);
+        bekle(5);
 
 
     }
---
+++ src/test/java/ders11_Cookies_WebTables/C02_CookieAutomation.java
@@ -0,0 +1,63 @@
+package ders11_Cookies_WebTables;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Cookie;
+import utilities.TestBase;
+
+import java.util.Set;
+
+public class C02_CookieAutomation extends TestBase {
+    @Test
+    public void cookieTesti(){
+        //Yeni bir class olusturun : cookiesAutomation
+        //1- Amazon anasayfaya gidin
+        driver.get("https://www.amazon.com");
+        //2- tum cookie’leri listeleyin
+        Set<Cookie> cookiesSeti= driver.manage().getCookies();
+        int siraNo=1;
+        for (Cookie eachCookie: cookiesSeti
+        ) {
+            System.out.println(siraNo+"- " + eachCookie);
+            siraNo++;
+        }
+        //3- Sayfadaki cookies sayisinin 5’den buyuk oldugunu test edin
+        Assert.assertTrue(cookiesSeti.size()>5);
+        //4- ismi i18n-prefs olan cookie degerinin USD oldugunu test edin
+        Cookie i18Cookie=driver.manage().getCookieNamed("i18n-prefs");
+        String i18Value=i18Cookie.getValue();
+        Assert.assertEquals("USD",i18Value);
+        //5- ismi “en sevdigim cookie” ve degeri “cikolatali” olan bir cookie olusturun
+        // ve sayfaya ekleyin
+        Cookie cookie= new Cookie("En sevdigim cookie","cikolatali");
+        driver.manage().addCookie(cookie);
+        //6- eklediginiz cookie’nin sayfaya eklendigini test edin
+        cookiesSeti= driver.manage().getCookies();
+        siraNo=1;
+        for (Cookie eachCookie: cookiesSeti
+        ) {
+            System.out.println(siraNo+"- " + eachCookie);
+            siraNo++;
+        }
+        Cookie enSevdigimCookie=driver.manage().getCookieNamed("En sevdigim cookie");
+        String enSevdigimCookiValue= enSevdigimCookie.getValue();
+        Assert.assertEquals("cikolatali",enSevdigimCookiValue);
+        //7- ismi skin olan cookie’yi silin ve silindigini test edin
+        cookiesSeti= driver.manage().getCookies();
+        int silmedenOnceCookieSayisi= cookiesSeti.size();
+        driver.manage().deleteCookieNamed("skin");
+        cookiesSeti= driver.manage().getCookies();
+        int silmedenSonraCookieSayisi= cookiesSeti.size();
+        siraNo=1;
+        for (Cookie eachCookie: cookiesSeti
+        ) {
+            System.out.println(siraNo+"- " + eachCookie);
+            siraNo++;
+        }
+        Assert.assertEquals(silmedenOnceCookieSayisi,silmedenSonraCookieSayisi+1);
+        //8- tum cookie’leri silin ve silindigini test edin
+        driver.manage().deleteAllCookies();
+        cookiesSeti= driver.manage().getCookies();
+        Assert.assertTrue(cookiesSeti.size()==0);
+    }
+}
